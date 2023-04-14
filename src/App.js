386f0c9659ecec0b0250fc8293275561f66424a0
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2023-04-14 19:10:05 +0100

    Added home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hi,</h1>
-        <h2>I'm Tsione, </h2>
-        <h2>Web developer</h2>
-        <p>Full-Stack Developer/ Front End Developer/ Back End Developer </p>
-      </header>
+      <header className="App-header"></header>
+
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>
+        <span>Hi,</span>
+        <br />
+        <span>I'm Tsione, </span>
+        <br />
+        <span>web developer</span>
+      </h1>
+      <p>Full-Stack Developer/ Front End Developer/ Back End Developer </p>
+      <button>Contact me!</button>
+    </div>
+  );
+};
+
+export default Home;
