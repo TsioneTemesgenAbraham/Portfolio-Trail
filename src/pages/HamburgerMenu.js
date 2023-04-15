34c4d9bcb222e98775f22a9db3c853eab0296780
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2023-04-15 01:10:16 +0100

    Added Hamburger Menu

---
+++ src/pages/HamburgerMenu.js
@@ -0,0 +1,14 @@
+import "./css/HamburgerMenu.css";
+
+const HamburgerMenu = () => {
+  return (
+    <div className="hamburger-menu">
+      <div className="hamburger-menu__line">About</div>
+      <div className="hamburger-menu__line">Skills</div>
+      <div className="hamburger-menu__line">Projects</div>
+      <div className="hamburger-menu__line">Contact</div>
+    </div>
+  );
+};
+
+export default HamburgerMenu;
