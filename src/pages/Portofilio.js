04da78d5f55b6bf1a02910b63070b3905adc3f36
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2023-04-15 01:10:32 +0100

    Added Portfolio

---
+++ src/pages/Portofilio.js
@@ -0,0 +1,16 @@
+const Portofilio = () => {
+  return (
+    <div>
+      <h1>My Portofilio</h1>
+      <p>
+        Hello and welcome to my web development portfolio! My name is Tsione
+        Abraham, and I'm a junior web developer. I specialize in creating
+        websites that are not only visually stunning but also user-friendly and
+        functional.
+      </p>
+      <button>See more!</button>
+    </div>
+  );
+};
+
+export default Portofilio;
