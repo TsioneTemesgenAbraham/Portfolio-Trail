8caaf4d9ce7278e30de1778f6d8a8fafe49d0158
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2023-04-25 03:02:11 +0100

    Added Contact.js file

---
+++ src/pages/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div>
+      <h2>Contact Details</h2>
+      <p>Name: Tsione Abraham</p>
+      <p>Phone Number: 07402662152</p>
+      <p>
+        Email:{" "}
+        <a href="tsionetemesgenabraham@gmail.com">
+          tsionetemesgenabraham@gmail.com
+        </a>{" "}
+      </p>
+    </div>
+  );
+}
