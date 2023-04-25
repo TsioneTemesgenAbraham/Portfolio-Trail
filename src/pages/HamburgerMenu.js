0b78f73bfae1126660f2c8664834292e291ad87d
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2023-04-25 02:05:28 +0100

    Added HamburgerMenu.js file

--- src/pages/HamburgerMenu.js
+++ src/pages/HamburgerMenu.js
@@ -1,13 +1,22 @@
+import React, { useState } from "react";
 import "./css/HamburgerMenu.css";
 
 const HamburgerMenu = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
-    <div className="hamburger-menu">
-      <div className="hamburger-menu__line">About</div>
-      <div className="hamburger-menu__line">Skills</div>
-      <div className="hamburger-menu__line">Projects</div>
-      <div className="hamburger-menu__line">Contact</div>
-    </div>
+    <>
+      <div class={`nav-links ${showMenu ? "show" : ""}`}>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/skills">Skills</a>
+        <a href="/contact">Contact</a>
+      </div>
+      <div className="hamburger-menu" onClick={() => setShowMenu(!showMenu)}>
+        <div className="hamburger-menu__line"></div>
+        <div className="hamburger-menu__line"></div>
+        <div className="hamburger-menu__line"></div>
+      </div>
+    </>
   );
 };
 
