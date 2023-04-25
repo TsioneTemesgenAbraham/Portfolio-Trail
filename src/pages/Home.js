1092cdd0ab0d81173ba2d0785def4d22a6c52221
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2023-04-25 02:08:50 +0100

    Amended Home.js file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,30 @@
+import React, { useState } from "react";
+import Portfolio from "./Portfolio";
+import HamburgerMenu from "./HamburgerMenu";
+
 const Home = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <div>
-      <h1>
-        <span>Hi,</span>
-        <br />
-        <span>I'm Tsione, </span>
-        <br />
-        <span>web developer</span>
-      </h1>
-      <p>Full-Stack Developer/ Front End Developer/ Back End Developer </p>
-      <button>Contact me!</button>
+    <div className="App">
+      <header className="App-header">
+        <nav className="navbar" showMenu={showMenu} setShowMenu={setShowMenu}>
+          <HamburgerMenu />
+        </nav>
+      </header>
+
+      <div>
+        <h1>
+          <span>Hi,</span>
+          <br />
+          <span>I'm Tsione, </span>
+          <br />
+          <span>web developer</span>
+        </h1>
+        <p>Full-Stack Developer/ Front End Developer/ Back End Developer </p>
+        <button>Contact me!</button>
+      </div>
+      <Portfolio />
     </div>
   );
 };
