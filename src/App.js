81242a9d1a90f37d119ad351e888d6792a1ce34b
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2023-04-25 03:01:51 +0100

    Added contact route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.css";
+import Contact from "./pages/Contact";
 
-function App() {
-  <Routes>
-    <Route path="/" element={<Home />} />
-  </Routes>;
-}
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
