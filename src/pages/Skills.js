8dc5abe76508372243a933fa3f00744ef415a788
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2024-01-09 21:46:52 +0000

    Added skiils page

---
+++ src/pages/Skills.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Skills() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Hello and welcome to my web development portfolio! My name is Tsione
+        Abraham, and I'm a junior web developer. I specialize in creating
+        websites that are not only visually stunning but also user-friendly and
+        functional.
+      </p>
+      <button>See more!</button>
+    </div>
+  );
+}
