70f143c8c1f6b7b61fe06bb5e7f2f1d8bdc35a33
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2024-01-09 21:45:30 +0000

    imported and routed skills and about pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.css";
+import Skills from "./pages/Skills";
+import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 
@@ -10,6 +12,8 @@ const App = () => (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/skills" element={<Skills />} />
       <Route path="*" component={<NotFound />} />
     </Routes>
   </Router>
